aa0c1ed4e35dca82d1384899bafe76afe8b7c8af
stefiscool <149964402+stefiscool@users.noreply.github.com>
2024-06-20 09:01:40 -0400

    added cannons and moving platforms, made the level a little easier

---
+++ parrallel-platformer-main/scenes/cannonball.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+
+
+func _on_body_entered(body):
+	if body.name == "Player" and Global.controllingleft == true:
+		get_tree().change_scene_to_file("res://scenes/game_over.tscn")
+	if body.name == "Secondplayer" and Global.controllingleft == false:
+		get_tree().change_scene_to_file("res://scenes/game_over.tscn")
