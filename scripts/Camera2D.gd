dead902f24fe1151c497841ed79763e0778bff5d
shouty-max <shouty2024@outlook.com>
2024-06-22 18:25:58 +1000

    Basic adaptive music!

--- scripts/Camera2D.gd
+++ scripts/Camera2D.gd
@@ -1,23 +1,27 @@
 extends Camera2D
 
+var MUSIC_MIN_VOLUME = -21.0
+var VOLUME_CHANGE_RATE = 42.0
+var heaven_target_volume = 0
+var hell_target_volume = MUSIC_MIN_VOLUME
+var heaven_music = AudioServer.get_bus_index("HeavenMusic")
+var hell_music = AudioServer.get_bus_index("HellMusic")
+
+func _ready():
+	set_music_mix(heaven_target_volume, hell_target_volume)
+
+func set_music_mix(heaven_db, hell_db):
+	AudioServer.set_bus_volume_db(heaven_music, heaven_db)
+	AudioServer.set_bus_volume_db(hell_music, hell_db)
 
 
-
-func _process(_delta):
+func _process(delta):
 	position.y = $"../Player".position.y
 	if Input.is_action_just_pressed("switch") and Global.controllingleft == true:
-		Global.controllingleft = false
-		global_position.x = $"../Player".global_position.x
-		global_position.y = $"../Player".global_position.y
-		$"../Player".modulate = Color(1, 1, 1, 1)
-		$"../Secondplayer".modulate = Color(1, 1, 1, Global.hidden_opacity)
+		try_swap_player($"../Player", $"../Secondplayer", false)
 
 	elif Input.is_action_just_pressed("switch") and Global.controllingleft == false:
-		Global.controllingleft = true
-		global_position.x = $"../Secondplayer".global_position.x
-		global_position.y = $"../Secondplayer".global_position.y
-		$"../Secondplayer".modulate = Color(1, 1, 1, 1)
-		$"../Player".modulate = Color(1, 1, 1, Global.hidden_opacity)
+		try_swap_player($"../Secondplayer", $"../Player", true)
 	
 	var viewport_size = get_viewport().get_visible_rect().size
 	var right_half = get_screen_center_position()
@@ -26,3 +30,36 @@ func _process(_delta):
 	left_half.x -= viewport_size.x/2
 	$"../HeavenBackground".position = right_half
 	$"../HellBackground".position = left_half
+	
+	var volume_change = VOLUME_CHANGE_RATE * delta
+	var heaven_music_actual_volume = AudioServer.get_bus_volume_db(heaven_music)
+	var heaven_new_volume = heaven_music_actual_volume
+	var hell_music_actual_volume = AudioServer.get_bus_volume_db(hell_music)
+	var hell_new_volume = hell_music_actual_volume
+	
+	if heaven_target_volume > heaven_music_actual_volume:
+		heaven_new_volume = min(heaven_target_volume, heaven_music_actual_volume + volume_change)
+	elif heaven_target_volume < heaven_music_actual_volume:
+		heaven_new_volume = max(heaven_target_volume, heaven_music_actual_volume - volume_change)
+		
+	if hell_target_volume > hell_music_actual_volume:
+		hell_new_volume = min(hell_target_volume, hell_music_actual_volume + volume_change)
+	elif hell_target_volume < hell_music_actual_volume:
+		hell_new_volume = max(hell_target_volume, hell_music_actual_volume - volume_change)
+	set_music_mix(heaven_new_volume, hell_new_volume)
+
+func try_swap_player(to_become, currently_is, become_left_not_right):
+	# test if non-active player would be able to move 0px (aka overlaps with an obstacle)
+	if not to_become.test_move(to_become.transform, Vector2()):
+		Global.controllingleft = become_left_not_right
+		if become_left_not_right:
+			hell_target_volume = 0
+			heaven_target_volume = MUSIC_MIN_VOLUME
+		else:
+			heaven_target_volume = 0
+			hell_target_volume = MUSIC_MIN_VOLUME
+		global_position.x = to_become.global_position.x
+		global_position.y = to_become.global_position.y
+		to_become.modulate = Color(1, 1, 1, 1)
+		currently_is.modulate = Color(1, 1, 1, Global.hidden_opacity)
+	
